4da52e9063141f17da38da07747b75917fc7f29d
Roy Jerman <sroyj@yahoo.com>
2017-09-16 09:52:41 -0400

    initial check-in

---
+++ React1/js/app.js
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+import ReactDOM from 'react_dom';
+
+class MyComponentClass extends React.Component {
+    render() {
+        return <h1>Hello world</h1>;
+    }
+};
+
+ReactDOM.render(
+  <MyComponentClass />,
+  document.getElementById('app')
+);
