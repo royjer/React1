f2150c8d695042ac8f90b6cef1b36df5e591f168
Roy Jerman <sroyj@yahoo.com>
2017-10-04 11:18:02 -0400

    commit staged

--- React1/js/index.js
+++ React1/js/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 class MyComponentClass extends React.Component {
     render() {
-        return <h1>Hello world2 Makin a little change......</h1>;
+        return <h1>Hello world2 Makin a little change..zzz....</h1>;
     }
 };
 
